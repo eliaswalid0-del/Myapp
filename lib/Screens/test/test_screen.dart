16aa9c79e72bcd9115be5b29ced5c92c5c42aad9
eliaswalid0-del <eliaswalid0@gmail.com>
2025-12-28 23:33:35 +0400

    add test screen

---
+++ lib/Screens/test/test_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TestScreen extends StatefulWidget {
+  @override
+  _TestScreenState createState() => _TestScreenState();
+}
+
+class _TestScreenState extends State<TestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Test Screen'),
+      ),
+      body: Center(
+        child: Text(
+          'GitHub Auto-Commit Works!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
